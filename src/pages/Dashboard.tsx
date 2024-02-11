d1196c7642a3fcc08d0e6c7dc80a191cfcba94af
P <jp.professional@outlook.com>
2024-02-11 20:40:58 -0300

    feat: render week days on availability section

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,8 @@
 import SideMenu from "@/components/molecules/SideMenu";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Plus } from "lucide-react";
 import { useState } from "react";
 
 const Default = () => (
@@ -11,6 +15,51 @@ const Default = () => (
   </div>
 );
 
+const weekDays = [
+  {
+    name: "Domingo",
+    start: "09:00",
+    end: "18:00",
+    enabled: false,
+  },
+  {
+    name: "Segunda",
+    start: "09:00",
+    end: "18:00",
+    enabled: true,
+  },
+  {
+    name: "Terça",
+    start: "09:00",
+    end: "18:00",
+    enabled: true,
+  },
+  {
+    name: "Quarta",
+    start: "09:00",
+    end: "18:00",
+    enabled: true,
+  },
+  {
+    name: "Quinta",
+    start: "09:00",
+    end: "18:00",
+    enabled: true,
+  },
+  {
+    name: "Sexta",
+    start: "09:00",
+    end: "18:00",
+    enabled: true,
+  },
+  {
+    name: "Sábado",
+    start: "09:00",
+    end: "18:00",
+    enabled: false,
+  },
+];
+
 const Availability = () => {
   return (
     <div className="flex items-center flex-col">
@@ -22,7 +71,32 @@ const Availability = () => {
         </div>
         <span>
           Informe a nós quando você normalmente estará disponível para
+          atendimentos.
         </span>
+        <span className="text-lg font-semibold mt-2">Horários da semana</span>
+        <div className="flex flex-col gap-2">
+          {weekDays.map((day) => (
+            <div className="flex gap-2">
+              <div className="flex items-center gap-2 w-1/3">
+                <Checkbox
+                  defaultChecked={day.enabled}
+                  className="data-[state=checked]:bg-blue-700"
+                />
+                <span className="text-md font-medium mt-2">{day.name}</span>
+              </div>
+              <div className="flex gap-2 w-1/3">
+                <Input value={day.start} />
+                <span className="self-center">-</span>
+                <Input value={day.end} />
+              </div>
+              <div className="w-1/3">
+                <Button variant="ghost" className="p-2 py-1">
+                  <Plus className="text-zinc-500" />
+                </Button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
