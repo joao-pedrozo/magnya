de55abaf0cf50c09492d8b9bfb3b8df08abe159e
P <jp.professional@outlook.com>
2024-02-11 20:05:23 -0300

    refactor: create separate component for SideMenu

---
+++ src/components/molecules/SideMenu.tsx
@@ -0,0 +1,94 @@
+import {
+  Collapsible,
+  CollapsibleTrigger,
+  CollapsibleContent,
+} from "@radix-ui/react-collapsible";
+import {
+  ChevronLeft,
+  LayoutDashboard,
+  CalendarDays,
+  Users,
+  DollarSign,
+  Clock,
+} from "lucide-react";
+import { Button } from "../ui/button";
+
+interface SideMenuProps {
+  isOpen: boolean;
+  setIsOpen: (value: boolean) => void;
+  setSelectedMenuItem: (value: string) => void;
+}
+
+function SideMenu({ isOpen, setIsOpen, setSelectedMenuItem }: SideMenuProps) {
+  return (
+    <Collapsible
+      open={isOpen}
+      onOpenChange={setIsOpen}
+      className="w-[350px] space-y-2 border-r"
+    >
+      <div className="flex items-center justify-between space-x-4">
+        <img src="../../logo.png" width={180} />
+
+        <CollapsibleTrigger asChild>
+          <Button variant="ghost" size="sm">
+            <ChevronLeft className="h-4 w-4" />
+            <span className="sr-only">Toggle</span>
+          </Button>
+        </CollapsibleTrigger>
+      </div>
+      <CollapsibleContent className="space-y-2">
+        <nav className="mt-5 ml-1">
+          <ul className="flex flex-col gap-2">
+            <li
+              onClick={() => {
+                setSelectedMenuItem("default");
+              }}
+            >
+              <a className="text-[18px] flex gap-2 p-2">
+                <LayoutDashboard />
+                <span className="font-semibold">Dashboard</span>
+              </a>
+            </li>
+            <li className="hover:text-blue-600 p-2">
+              <a className="text-[18px] flex gap-2">
+                <CalendarDays />
+                <span className="font-semibold">Agenda</span>
+              </a>
+            </li>
+            <li className="hover:text-blue-600 p-2">
+              <a href="#" className="text-[18px] flex gap-2">
+                <Users />
+                <span className="font-semibold">Clientes</span>
+              </a>
+            </li>
+            <li className="hover:text-blue-600 p-2">
+              <a href="#" className="text-[18px] flex gap-2">
+                <DollarSign className="ml-[-2px]" />
+                <span className="font-semibold">Pagamentos</span>
+              </a>
+            </li>
+            <li
+              className="hover:text-blue-600 p-2"
+              onClick={() => {
+                setSelectedMenuItem("availability");
+              }}
+            >
+              <a className="text-[18px] flex gap-2">
+                <Clock className="ml-[-2px]" />
+                <span className="font-semibold">Disponibilidade</span>
+              </a>
+            </li>
+            <li className="hover:text-blue-600 p-2">
+              <a href="#" className="text-[18px] flex gap-2">
+                <LayoutDashboard />
+                <span className="font-semibold">Configurações</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </CollapsibleContent>
+    </Collapsible>
+  );
+}
+
+export default SideMenu;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,19 +1,5 @@
-import {
-  ChevronLeft,
-  LayoutDashboard,
-  CalendarDays,
-  Users,
-  DollarSign,
-  Clock,
-} from "lucide-react";
-import React from "react";
-
-import { Button } from "@/components/ui/button";
-import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from "@/components/ui/collapsible";
+import SideMenu from "@/components/molecules/SideMenu";
+import { useState } from "react";
 
 const Default = () => (
   <div>
@@ -43,78 +29,16 @@ const Availability = () => {
 };
 
 function Dashboard() {
-  const [isOpen, setIsOpen] = React.useState(true);
-  const [selectedMenuItem, setSelectedMenuItem] = React.useState("default");
+  const [isOpen, setIsOpen] = useState(true);
+  const [selectedMenuItem, setSelectedMenuItem] = useState("default");
 
   return (
     <div className="py-4 px-8 flex gap-8">
-      <Collapsible
-        open={isOpen}
-        onOpenChange={setIsOpen}
-        className="w-[350px] space-y-2 border-r"
-      >
-        <div className="flex items-center justify-between space-x-4">
-          <img src="../../logo.png" width={180} />
-
-          <CollapsibleTrigger asChild>
-            <Button variant="ghost" size="sm">
-              <ChevronLeft className="h-4 w-4" />
-              <span className="sr-only">Toggle</span>
-            </Button>
-          </CollapsibleTrigger>
-        </div>
-        <CollapsibleContent className="space-y-2">
-          <nav className="mt-5 ml-1">
-            <ul className="flex flex-col gap-2">
-              <li
-                onClick={() => {
-                  setSelectedMenuItem("default");
-                }}
-              >
-                <a className="text-[18px] flex gap-2 p-2">
-                  <LayoutDashboard />
-                  <span className="font-semibold">Dashboard</span>
-                </a>
-              </li>
-              <li className="hover:text-blue-600 p-2">
-                <a className="text-[18px] flex gap-2">
-                  <CalendarDays />
-                  <span className="font-semibold">Agenda</span>
-                </a>
-              </li>
-              <li className="hover:text-blue-600 p-2">
-                <a href="#" className="text-[18px] flex gap-2">
-                  <Users />
-                  <span className="font-semibold">Clientes</span>
-                </a>
-              </li>
-              <li className="hover:text-blue-600 p-2">
-                <a href="#" className="text-[18px] flex gap-2">
-                  <DollarSign className="ml-[-2px]" />
-                  <span className="font-semibold">Pagamentos</span>
-                </a>
-              </li>
-              <li
-                className="hover:text-blue-600 p-2"
-                onClick={() => {
-                  setSelectedMenuItem("availability");
-                }}
-              >
-                <a className="text-[18px] flex gap-2">
-                  <Clock className="ml-[-2px]" />
-                  <span className="font-semibold">Disponibilidade</span>
-                </a>
-              </li>
-              <li className="hover:text-blue-600 p-2">
-                <a href="#" className="text-[18px] flex gap-2">
-                  <LayoutDashboard />
-                  <span className="font-semibold">Configurações</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </CollapsibleContent>
-      </Collapsible>
+      <SideMenu
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        setSelectedMenuItem={setSelectedMenuItem}
+      />
       {selectedMenuItem === "default" && <Default />}
       {selectedMenuItem === "availability" && <Availability />}
     </div>
