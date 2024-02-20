f19f7840fa105ec44508650d2baa55ce57fe4365
P <jp.professional@outlook.com>
2024-02-19 23:38:10 -0300

    feat: add calendar and render it on schedule page

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -2,12 +2,14 @@ import { supabase } from "@/supabase";
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { Database } from "../../database.types";
+import { Calendar } from "@/components/ui/calendar";
 
 type Specialist = Database["public"]["Tables"]["specialists"]["Row"];
 
 const Schedule = () => {
   const [loading, setLoading] = useState(true);
   const [specialist, setSpecialist] = useState<Specialist | null>(null);
+  const [date, setDate] = useState<Date | undefined>(new Date());
 
   const location = useLocation();
   const navigate = useNavigate();
@@ -38,11 +40,17 @@ const Schedule = () => {
   }
 
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <h1 className="text-4xl font-bold text-center mt-4">Schedule Page</h1>
       <p className="mt-2 text-center text-xl">
         Welcome to the schedule page for <b>{specialist.username}</b>
       </p>
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border mt-4"
+      />
     </div>
   );
 };
