dbb2e1a648e3c06c28d414cbe2e98885d7d5e244
P <jp.professional@outlook.com>
2024-03-22 01:38:58 -0300

    feat: fetch and render appointments

--- src/components/organisms/dashboard/Agenda/components/TableData.tsx
+++ src/components/organisms/dashboard/Agenda/components/TableData.tsx
@@ -17,53 +17,27 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { useQuery } from "@tanstack/react-query";
+import { supabase } from "@/supabase";
 
-const mockData = [
-  {
-    cliente: "John Doe",
-    data: "12/10/2021",
-    hora: "14:00",
-    valor: "R$ 150,00",
-    status: "Confirmado",
-  },
-  {
-    cliente: "Jane Smith",
-    data: "15/10/2021",
-    hora: "10:30",
-    valor: "R$ 200,00",
-    status: "Confirmado",
-  },
-  {
-    cliente: "Michael Johnson",
-    data: "18/10/2021",
-    hora: "16:45",
-    valor: "R$ 120,00",
-    status: "Pendente",
-  },
-  {
-    cliente: "Emily Davis",
-    data: "20/10/2021",
-    hora: "09:15",
-    valor: "R$ 180,00",
-    status: "Confirmado",
-  },
-  {
-    cliente: "David Wilson",
-    data: "22/10/2021",
-    hora: "13:30",
-    valor: "R$ 90,00",
-    status: "Cancelado",
-  },
-  {
-    cliente: "Olivia Taylor",
-    data: "25/10/2021",
-    hora: "11:00",
-    valor: "R$ 150,00",
-    status: "Confirmado",
-  },
-];
+const mockSession = {
+  id: 1,
+};
 
 export default function TableData() {
+  const appointments = useQuery({
+    queryKey: ["appointments"],
+    queryFn: async () =>
+      await supabase
+        .from("appointments")
+        .select(
+          `
+            *, clients(*) 
+          `
+        )
+        .eq("specialist_id", mockSession.id),
+  });
+
   return (
     <Table className="border rounded-xl border-separate border-spacing-x-[28px] border-spacing-y-[8px]">
       <TableHeader>
@@ -76,47 +50,58 @@ export default function TableData() {
           <TableHead>Ações</TableHead>
         </TableRow>
       </TableHeader>
-      <TableBody>
-        {mockData.map((item, index) => (
-          <TableRow key={index}>
-            <TableCell>
-              <span className="font-semibold">{item.cliente}</span>
-            </TableCell>
-            <TableCell>
-              <span>{item.data}</span>
-            </TableCell>
-            <TableCell>
-              <span>{item.hora}</span>
-            </TableCell>
-            <TableCell>
-              <span>{item.valor}</span>
-            </TableCell>
-            <TableCell>
-              <Badge className="bg-blue-600 hover:bg-blue-600">
-                Confirmado
-              </Badge>
-            </TableCell>
-            <TableCell>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="ghost" className="h-8 w-8 p-0">
-                    <span className="sr-only">Open menu</span>
-                    <Ellipsis className="h-4 w-4" />
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  <DropdownMenuItem className="font-semibold text-yellow-500 cursor-pointer">
-                    Reagendar
-                  </DropdownMenuItem>
-                  <DropdownMenuItem className="font-semibold text-red-500 cursor-pointer">
-                    Cancelar
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
+      {appointments.isLoading ? (
+        <p>Carregando...</p>
+      ) : (
+        <TableBody>
+          {appointments.data?.data?.map((item, index) => (
+            <TableRow key={index}>
+              <TableCell>
+                <span className="font-semibold">
+                  {item.clients.first_name} {item.clients.last_name}
+                </span>
+              </TableCell>
+              <TableCell>
+                <span>{item.date}</span>
+              </TableCell>
+              <TableCell>
+                <span>{item.time}</span>
+              </TableCell>
+              <TableCell>
+                <span>
+                  {new Intl.NumberFormat("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  }).format(item.value)}
+                </span>
+              </TableCell>
+              <TableCell>
+                <Badge className="bg-blue-600 hover:bg-blue-600">
+                  Confirmado
+                </Badge>
+              </TableCell>
+              <TableCell>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" className="h-8 w-8 p-0">
+                      <span className="sr-only">Open menu</span>
+                      <Ellipsis className="h-4 w-4" />
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuItem className="font-semibold text-yellow-500 cursor-pointer">
+                      Reagendar
+                    </DropdownMenuItem>
+                    <DropdownMenuItem className="font-semibold text-red-500 cursor-pointer">
+                      Cancelar
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      )}
     </Table>
   );
 }
