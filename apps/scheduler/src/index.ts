da0dcd8bc4e779c1ef2b5d435a9675a9e44ba274
P <jp.professional@outlook.com>
2024-03-24 18:42:45 -0300

    feat: add scheduler to make appointments completed

--- apps/scheduler/src/index.ts
+++ apps/scheduler/src/index.ts
@@ -1,6 +1,25 @@
 import * as cron from "node-cron";
+import { supabase } from "./supabase";
 
-cron.schedule("* * * * *", () => {
-  console.log("Running appointment verification job...");
-  // Your verification logic here
+require("dotenv").config({ path: `../../.env.local` });
+
+console.log(process.env.SUPABASE_PROJECT_URL);
+
+// Run every 5 seconds
+cron.schedule("*/5 * * * * *", async () => {
+  console.log("running a task every 5 seconds");
+
+  const updateStatus = async () =>
+    await supabase
+      .from("appointments")
+      .update({
+        status: "completed",
+      })
+      .eq("status", "pending")
+      .lte("scheduled_date", new Date().toISOString())
+      .select();
+
+  const { count } = await updateStatus();
+
+  console.log(`${count} updated`);
 });
